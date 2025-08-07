6bf73172c7e41a04a73641557823b6c29565c3fa
245623733333 <your-sriamshas@gmail.com>
2025-08-07 13:33:57 +0530

    Fix: use CDN import for smart-widget-handler

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import { ready } from "https://unpkg.com/smart-widget-handler@latest/dist/index.js";
 
-// Wait until DOM is fully loaded
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
   ready();
 });

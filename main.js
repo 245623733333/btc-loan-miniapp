a82fe1345d5dacd0b4e1b364c5a24b51c44db8da
245623733333 <your-sriamshas@gmail.com>
2025-08-07 12:50:31 +0530

    Fix ready() import for smart-widget-handler

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-import { ready } from './node_modules/smart-widget-handler/dist/index.js';
+import { ready } from "https://unpkg.com/smart-widget-handler@latest/dist/index.js";
 
 // Wait until DOM is fully loaded
 window.addEventListener('DOMContentLoaded', () => {
-  // Do any setup if needed
-
-  // Then call ready
   ready();
 });

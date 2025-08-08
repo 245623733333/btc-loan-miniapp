1723156814a678a78af857e8c3306a8cd70fffc0
245623733333 <your-sriamshas@gmail.com>
2025-08-08 08:36:01 +0530

    Update UI and fix user metadata integration

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import SWHandler from 'https://cdn.jsdelivr.net/npm/smart-widget-handler/+esm';
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
   console.log('[CLIENT] Calling ready()...');
-  SWHandler.client.ready();
+  await SWHandler.client.ready();
 
   SWHandler.client.listen((data) => {
     console.log('[CLIENT] Received data from host:', data);
